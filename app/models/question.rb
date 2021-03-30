57343a960d96a04c31bb52a1d90dc18f1df1e4c4
Hannah Voget <hannah@voget.de>
2021-03-30 14:19:30 +0200

    Add Question Model

---
+++ app/models/question.rb
@@ -0,0 +1,3 @@
+class Question < ApplicationRecord
+  belongs_to :board
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_120056) do
+ActiveRecord::Schema.define(version: 2021_03_30_121706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2021_03_30_120056) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.bigint "board_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_questions_on_board_id"
+  end
+
+  add_foreign_key "questions", "boards"
 end
