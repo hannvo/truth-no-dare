801f9c39de81706ffdc3e3f5e9811d4981228c93
Hannah Voget <hannah@voget.de>
2021-03-31 13:41:18 +0200

    Rename question column in questions to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_122058) do
+ActiveRecord::Schema.define(version: 2021_03_31_113218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_122058) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "question"
+    t.string "text"
     t.bigint "board_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
