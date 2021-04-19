1be31498d1b7f1a07d3c1f8c92c1b417cc856fc1
Hannah Voget <hannah@voget.de>
2021-04-19 21:25:20 +0200

    Create homepage and boards#show

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,20 @@
+class BoardsController < ApplicationController
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    @board.save ? (redirect_to board_path(@board)) : (render :new)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :path)
+  end
+end
