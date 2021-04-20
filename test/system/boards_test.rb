4233ba53a3983fff75bf873151511e4f5665c4b9
Hannah Voget <hannah@voget.de>
2021-04-20 15:53:57 +0200

    Remove unnecessary comments

--- test/system/boards_test.rb
+++ test/system/boards_test.rb
@@ -8,18 +8,15 @@ class BoardsTest < ApplicationSystemTestCase
 
   test "lets a visitor create a new board" do
     visit root_url
-    # save_and_open_screenshot
 
     fill_in "board_title", with: "Test Title"
     fill_in "board_path", with: "truthnodare"
     # save_and_open_screenshot
 
     click_on 'Create Board'
-    # save_and_open_screenshot
 
     # Should be redirected to boards show page
     assert_equal "/boards/truthnodare", page.current_path
-    save_and_open_screenshot
     assert_text "Test Title"
   end
 end
