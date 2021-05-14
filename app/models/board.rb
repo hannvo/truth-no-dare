e3ac34438e436c878aeb34bee0089f055ba33d74
Hannah Voget <hannah@voget.de>
2021-05-14 22:33:22 +0200

    Fix theme default setting

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,10 +5,18 @@ class Board < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, through: :questions, dependent: :destroy
 
+  before_validation :set_default_theme
+
   validates :title, presence: true, length: { maximum: 40 }
   validates :theme, presence: true, inclusion: { in: ['fun'] }
 
   def open_questions(num)
     questions.left_outer_joins(:answers).where("answers.question_id is null").limit(num).to_a
+  end
+
+  private
+
+  def set_default_theme
+    self.theme = 'fun' if theme.nil?
   end
 end
