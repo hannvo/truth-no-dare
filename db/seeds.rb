46680c25f8a18726718b75212ca50e93a93eeffb
Hannah Voget <hannah@voget.de>
2021-05-29 17:15:22 +0200

    Add test Board to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+test_board = Board.new(title: 'test')
+test_board.save
+
+q1 = Question.new
+q1.text = 'This is a test question. Is there something you need to know?'
+q1.board = test_board
+q1.save
